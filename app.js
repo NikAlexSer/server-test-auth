f6bb6f215c009ebf32e6d35ec786e814ab3a3ea2
NikAlexSer <alexandernikser@gmail.com>
2019-01-14 13:16:18 +0500

    test

--- app.js
+++ app.js
@@ -28,8 +28,10 @@ router.get('/auth', (req, res, next) => {
     res.send(req.query)
 })
 router.post('/test', (req, res, next) => {
-    console.log(req)
-    res.send(JSON.stringify(req))
+    console.log(req.body)
+    console.log(req.payload)
+    console.log(req.data)
+    res.send('ziga o/')
 })
 
 app.listen(port, (err) => {
